2e4161974498898a0b7eb5a9f53c54827334adef
Sayan Mondal <84088586+sayan-embee@users.noreply.github.com>
2021-10-11 17:25:16 +0530

    Update adaptiveCardEmailTemplate.ts

--- Source/CompanyCommunicator/ClientApp/src/components/AdaptiveCard/adaptiveCardEmailTemplate.ts
+++ Source/CompanyCommunicator/ClientApp/src/components/AdaptiveCard/adaptiveCardEmailTemplate.ts
@@ -33,22 +33,6 @@ export const getInitAdaptiveCardEmailTemplate = (t: TFunction) => {
                         "wrap": true
                     },
                     {
-                        "type": "Image",
-                        "spacing": "Default",
-                        "url": "",
-                        "msTeams": {
-                            "allowExpand": true
-                        },
-                        "selectAction": {
-                            "type": "Action.OpenUrl",
-                            "title": "Image",
-                            "url": ""
-                          },
-                        "size": "Stretch",
-                        "width": "300px",
-                        "altText": ""
-                    },
-                    {
                         "type": "TextBlock",
                         "wrap": true,
                         "text": ""
@@ -76,51 +60,66 @@ export const getInitAdaptiveCardEmailTemplate = (t: TFunction) => {
 
 }
 
-export const getCardTitleEmailTemplate = (card: any) => {
-    return card.body[0].text;
-}
-
-export const setCardTitleEmailTemplate = (card: any, title: string) => {
-    card.body[0].text = title;
-}
-
-export const getCardImageLinkEmailTemplate = (card: any) => {
-    return card.body[1].url;
-}
-
-export const getCardImageselectActionLinkEmailTemplate = (card: any) => {
-    return card.body[1].selectAction.url;
-}
-
-export const setCardImageLinkEmailTemplate = (card: any, imageLink?: string) => {
-    card.body[1].url = imageLink;
-    card.body[1].selectAction.url = imageLink;
-}
-
-export const getCardFileNameEmailTemplate = (card: any) => {
-    return card.body[2].text;
-}
-
-export const setCardFileNameEmailTemplate = (card: any, link?: string) => {
-    card.body[2].text = link;
-}
 
 
-export const getCardSummaryEmailTemplate = (card: any) => {
-    return card.body[3].text;
+
+export const setCardEmailTemplate = (card: any, imageLink?: string, title?: any, author?: any, emailLink?:any, summary?:any) => {
+    var jsonArray = new Array();
+    jsonArray.push(`{
+            "type": "TextBlock",
+            "weight": "Bolder",
+            "text": "${title}",
+            "size": "ExtraLarge",
+            "wrap": true
+        }`)
+        if(imageLink !== ""){
+            jsonArray.push(`{
+                "type": "Image",
+                "spacing": "Default",
+                "url": "${imageLink}",
+                "msTeams": {
+                    "allowExpand": true
+                },
+                "selectAction": {
+                    "type": "Action.OpenUrl",
+                    "title": "Image",
+                    "url": "${imageLink}"
+                  },
+                "size": "Stretch",
+                "width": "300px",
+                "altText": ""
+            }`)
+        }
+        
+        jsonArray.push(`{
+            "type": "TextBlock",
+            "wrap": true,
+            "size": "Small",
+            "weight": "Lighter",
+            "text": "${emailLink}"
+        }`)
+        jsonArray.push(`{
+            "type": "TextBlock",
+            "wrap": true,
+            "size": "Small",
+            "weight": "Lighter",
+            "text": "${summary}"
+        }`)
+        jsonArray.push(`{
+            "type": "TextBlock",
+            "wrap": true,
+            "size": "Small",
+            "weight": "Lighter",
+            "text": "${author}"
+        }`)
+
+        var jsonQuestionCard = "[" + jsonArray.join() + "]";
+        card.body = JSON.parse(jsonQuestionCard);
+        return card.body[0];
+
 }
 
-export const setCardSummaryEmailTemplate = (card: any, summary?: string) => {
-    card.body[3].text = summary;
-}
 
-export const getCardAuthorEmailTemplate = (card: any) => {
-    return card.body[4].text;
-}
-
-export const setCardAuthorEmailTemplate = (card: any, author?: string) => {
-    card.body[4].text = author;
-}
 
 
 
